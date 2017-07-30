b2f32ae502958f9b1b6c061c0be9b587a1c9a339
DESKTOP-U7M001C\ozsoy <ozsoymrve@gmail.com>
2017-07-30 13:35:36 +0300

    commit 2

--- src/com/trello/PlayWithString/mainFunction.java
+++ src/com/trello/PlayWithString/mainFunction.java
@@ -6,8 +6,8 @@ public class mainFunction {
 
 	public static void main(String[] args) {
 		String s1="Computer Science";
-		int value=307;
-		String s2=s1+" x";
+		int x=307;
+		String s2=s1+" "+x;
 		String s3=s1.substring(7,10);
 		String s4="is fun";
 		String s5=s2+" "+s4;
@@ -18,13 +18,12 @@ public class mainFunction {
 			System.out.println("s4="+s4);	
 			System.out.println("s5="+s5);	
 			
-			int x=3;
+			 x=3;
 			int y=5;
-			int total=x+y;
-			String s6="value of x:"+x+"\nvalue of y:"+y+"\ntotal:"+Integer.toString(total);
+			String s6="value of x:"+x+"\tvalue of y:"+y+"\ttotal";
 			
-			String s7=Integer.toString(total)+x+y;
-			String s8=" "+"x:"+x+"y:"+y+"total:"+total;
+			String s7="total"+x+y;
+			String s8=" "+"x:"+x+"y:"+y+"total";
 			
 			System.out.println("s6="+s6);	
 			System.out.println("s7="+s7);	
